d709bd4405dfbe8cc7ac1defab9c3e60d477ab56
vashvets <vashvets@outlook.com>
2021-05-03 12:41:26 +0000

    Допил конфигов

--- 1.sh
+++ 1.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-apt update
-apt-get upgrade -y
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
- sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io
- sudo docker run
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo docker run hello-world
+# Linux post-install
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
  sudo chmod +x /usr/local/bin/docker-compose
+ cd ~
+ sudo mkdir ./compose
  printf "version: '3.1'
 
 services:
@@ -42,7 +47,7 @@ services:
       MYSQL_PASSWORD: examplepass
       MYSQL_RANDOM_ROOT_PASSWORD: '1'
     volumes:
-      - db:/var/lib/mysql" >docker-compose.yml
+      - db:/var/lib/mysql" > ./compose/docker-compose.yml
 sudo docker-compose build
 sudo docker-compose up
 
--- awaterr.tf
+++ awaterr.tf
@@ -54,15 +54,9 @@ resource "aws_security_group" "terra1-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
+        from_port = 0
+        to_port = 0
+        protocol = -1
         cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
@@ -76,8 +70,4 @@ resource "aws_instance" "testubunta2" {
     vpc_security_group_ids = [aws_security_group.terra1-sg.id]
     key_name = "shtirliz"
     user_data = file("1.sh")
-
-    tags = {
-      "Name" = "terra1-ubunta1"
-    }
 }
